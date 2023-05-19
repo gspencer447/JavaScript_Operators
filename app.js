1f50bec97fc8cf1aee78181cc289613c6893db9c
Graham Spencer <gspencer447@yahoo.com>
2023-05-19 13:46:25 -0500

    fixed answers below the console.log

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ console.log(
 );
 
 // Exercise 1
-//var a = 20;
-//var b = 4;
-// var add = a + b;
-// var minus = a - b;
-// var multiply = a * b;
-// var dividing = a / b;
-// console.log(add);
+var a = 20;
+var b = 4;
+var add = a + b;
+var minus = a - b;
+var multiply = a * b;
+var dividing = a / b;
+console.log(add);
 
 // YOUR CODE HERE
 
@@ -22,28 +22,43 @@ let isPresent = true;
 let firstName = "Frodo";
 let lastName = "Baggins";
 console.log(num + str); 
+//1111
 console.log(num + str2);
+//11eleven
 console.log(num + isPresent);
+//12
 console.log(firstName + num);
+//Frodo11
 console.log(isPresent + str);
+//true11
 console.log(firstName + lastName);
+//FrodoBaggins
 
 // YOUR CODE HERE
 
 // Exercise 3
-// let val = 5;
-// let str3 = "5";
-// let str4 = "five";
-// let isPresent2 = false;
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
 
-// console.log(val == str3);
-// console.log(val === str3);
-// console.log(!isPresent2);
-// console.log("eleven" == str4 && val >= str3);
-// console.log(!isPresent2 || isPresent2);
-// console.log(0 == false);
-// console.log(0 === false);
-// console.log(0 != false);
-// console.log(0 !== false);
+console.log(val == str3);
+//true
+console.log(val === str3);
+//false
+console.log(!isPresent2);
+//true
+console.log("eleven" == str4 && val >= str3);
+//false
+console.log(!isPresent2 || isPresent2);
+//true
+console.log(0 == false);
+//true
+console.log(0 === false);
+//false
+console.log(0 != false);
+//false
+console.log(0 !== false);
+//true
 
 // YOUR CODE HERE
