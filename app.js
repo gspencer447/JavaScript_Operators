5ade4964daf3c8f38f7844a02ec6fe44677cc906
Graham Spencer <gspencer447@yahoo.com>
2023-05-19 13:40:30 -0500

    updated console logs

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ console.log(
 );
 
 // Exercise 1
-// var a = 20;
-// var b = 4;
+//var a = 20;
+//var b = 4;
 // var add = a + b;
 // var minus = a - b;
 // var multiply = a * b;
@@ -14,22 +14,36 @@ console.log(
 
 // YOUR CODE HERE
 
-// Exercise 2
+//Exercise 2
 let num = 11;
 let str = "11";
 let str2 = "eleven";
 let isPresent = true;
 let firstName = "Frodo";
 let lastName = "Baggins";
+console.log(num + str); 
+console.log(num + str2);
 console.log(num + isPresent);
+console.log(firstName + num);
+console.log(isPresent + str);
+console.log(firstName + lastName);
 
 // YOUR CODE HERE
 
 // Exercise 3
-let val = 5;
-let str3 = "5";
-let str4 = "five";
-let isPresent2 = false;
-console.log(val == str3);
+// let val = 5;
+// let str3 = "5";
+// let str4 = "five";
+// let isPresent2 = false;
+
+// console.log(val == str3);
+// console.log(val === str3);
+// console.log(!isPresent2);
+// console.log("eleven" == str4 && val >= str3);
+// console.log(!isPresent2 || isPresent2);
+// console.log(0 == false);
+// console.log(0 === false);
+// console.log(0 != false);
+// console.log(0 !== false);
 
 // YOUR CODE HERE
